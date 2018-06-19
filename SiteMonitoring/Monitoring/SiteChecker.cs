703dce21cd5b9a0f7e389e9344fcfd85b45b8561
gleb.kucherenko@freemake.com <gleb.kucherenko@freemake.com>
2018-06-19 09:49:18 +0300

    draw table

--- SiteMonitoring/Monitoring/SiteChecker.cs
+++ SiteMonitoring/Monitoring/SiteChecker.cs
@@ -9,21 +9,21 @@ namespace Monitoring
         public void CheckAsync(string url, Action<bool> callback)
         {
             Task.Run(() =>
-           {
-               try
-               {
-                   var request = WebRequest.Create(url);
-                   using (var response = (HttpWebResponse)request.GetResponse())
-                   {
-                       callback(response != null && response.StatusCode == HttpStatusCode.OK);
-                   }
-               }
-               catch
-               (WebException)
-               {
-                   callback(false);
-               }
-           });
+            {
+                try
+                {
+                    var request = WebRequest.Create(url);
+                    using (var response = (HttpWebResponse)request.GetResponse())
+                    {
+                        callback(response != null && response.StatusCode == HttpStatusCode.OK);
+                    }
+                }
+                catch
+                (WebException)
+                {
+                    callback(false);
+                }
+            });
         }
     }
 }
